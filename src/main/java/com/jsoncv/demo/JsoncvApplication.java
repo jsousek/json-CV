5c5cfb93198206426626e0c401e4782e32a0fecb
Jiri Sousek <jirisousek@outlook.com>
2019-02-23 15:33:26 +0100

    project generated, postgres added

---
+++ src/main/java/com/jsoncv/demo/JsoncvApplication.java
@@ -0,0 +1,13 @@
+package com.jsoncv.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoncvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsoncvApplication.class, args);
+    }
+
+}
